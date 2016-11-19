8787a885273e4be40fbc58c04ab2482f3daa9e85
HB07-05 <HB07-05@HB07_05>
2016-11-19 17:51:29 +0900

    Add Servlet

--- src/main/java/com/hanbit/jsp/WelcomeServlet.java
+++ src/main/java/com/hanbit/jsp/WelcomeServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class welcomeServlet extends HttpServlet {
+public class WelcomeServlet extends HttpServlet {
 	
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
