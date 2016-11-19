37d8b97961dda13a0189108aec90c2766a88efe7
HB07-05 <HB07-05@HB07_05>
2016-11-19 17:39:30 +0900

    First Commit

---
+++ src/main/java/com/hanbit/jsp/welcomeServlet.java
@@ -0,0 +1,33 @@
+package com.hanbit.jsp;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class welcomeServlet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		//resp.getOutputStream().println("Welcome Service");
+		resp.getOutputStream().println("Welcome " + req.getMethod());
+		resp.flushBuffer();
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		resp.getOutputStream().println("Welcome Get");
+		resp.flushBuffer();
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		resp.getOutputStream().println("Welcome Post");
+		resp.flushBuffer();
+	}
+}
